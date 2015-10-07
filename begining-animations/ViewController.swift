8460a495e15143073c9c04f31fe7404e528c7a3a
Jason Shultz <jasshultz@gmail.com>
2015-10-07 08:22:16 -0600

    fading the image in when the app starts

--- begining-animations/ViewController.swift
+++ begining-animations/ViewController.swift
@@ -35,17 +35,31 @@ class ViewController: UIViewController {
     
     // begin animating the image in from the left.
     
-    override func viewDidLayoutSubviews() {
-        camelImage.center = CGPointMake(camelImage.center.x - 400, camelImage.center.y)
-    }
-    
-    override func viewDidAppear(animated: Bool) {
-        UIView.animateWithDuration(1) { () -> Void in
-            self.camelImage.center = CGPointMake(self.camelImage.center.x + 400, self.camelImage.center.y)
-        }
-    }
+//    override func viewDidLayoutSubviews() {
+//        camelImage.center = CGPointMake(camelImage.center.x - 400, camelImage.center.y)
+//    }
+//    
+//    override func viewDidAppear(animated: Bool) {
+//        UIView.animateWithDuration(1) { () -> Void in
+//            self.camelImage.center = CGPointMake(self.camelImage.center.x + 400, self.camelImage.center.y)
+//        }
+//    }
     
     // end animating the image in from the left.
+    
+    // begin fading the image in
+    
+        override func viewDidLayoutSubviews() {
+            camelImage.alpha = 0
+        }
+    
+        override func viewDidAppear(animated: Bool) {
+            UIView.animateWithDuration(1) { () -> Void in
+                self.camelImage.alpha = 1
+            }
+        }
+    
+    // end fading the image in.
 
 }
 
