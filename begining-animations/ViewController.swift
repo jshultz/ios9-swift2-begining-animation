e4bcab300e2df6735d0ec9960e80c5796ed5f4f3
Jason Shultz <jasshultz@gmail.com>
2015-10-07 08:09:02 -0600

    init commit

--- begining-animations/ViewController.swift
+++ begining-animations/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var filename:String = "Camel_3-1.png"
 
+    @IBOutlet weak var camelImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func updateImage(sender: AnyObject) {
+        
+        filename = filename == "Camel_3-1.png" ? "Camel_3-2.png" : "Camel_3-1.png"
+        
+        
+        camelImage.image = UIImage(named: filename)
+    }
 
 }
 
